f64b517e19aa6e2cca4b8a0ea5278f5556c9a094
K7IT2024 <dhanyavani1991@gmail.com>
2026-06-12 09:12:05 +0530

    Initial CognivueX frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8090/api",
+  baseURL: "https://cognivuex-backend-1.onrender.com/api",
 });
 
 export const getLatestReport = () =>
